a9e47155e9218956b2aa7b9098170eb225aea4d2
Wriyer <71541423+MKaxie@users.noreply.github.com>
2021-05-21 14:29:44 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import torch
+import numpy as np
+from model.ACGAN import ICONGAN
+from utils.configs import Configs
+
+
+args = Configs()
+icongan = ICONGAN(args)
+icongan.train()
+
+
+test_labels = np.random.randint(0,2,(25,26))
+
+tests = torch.tensor(test_labels)
+
+icongan.generate(tests)
+
+
+
+
+
